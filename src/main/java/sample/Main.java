fb240bce083e204c05146dc7cdafb54cfed2d292
artursvaitilavics <artyvt@gmail.com>
2020-09-26 15:40:20 +0300

    Authors DAO working

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import configuration.DbSessionHolder;
+import dao.AuthorDao;
+import entity.Author;
 
 public class Main { //extends Application {
 
@@ -14,7 +16,13 @@ public class Main { //extends Application {
 
     public static void main(String[] args) {
         DbSessionHolder.getInstance();
-     //   launch(args);
+        //   launch(args);
+        Author author00001 = new Author();
+        author00001.setName("Inddrikis");
+        author00001.setSurName("Neredzigais");
+        AuthorDao authorDao = new AuthorDao();
+        authorDao.createAuthor(author00001);
+
         //TODO DB operations
         DbSessionHolder.shutdown();
 
