f68fe339dbae387211013f68287c7824ae005b32
artursvaitilavics <artyvt@gmail.com>
2020-09-26 13:49:00 +0300

    Book Mapped,

--- src/main/java/entity/Book.java
+++ src/main/java/entity/Book.java
@@ -8,4 +8,56 @@ import java.util.Set;
 @Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "bookId")
+    private Integer id;
+    @Column(name = "bookTitle")
+    private String title;
+    @Column(name = "bookDescription")
+    private String description;
+    @Column(name = "authorId")
+    private Integer authorId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", authorId=" + authorId +
+                '}';
+    }
 }
